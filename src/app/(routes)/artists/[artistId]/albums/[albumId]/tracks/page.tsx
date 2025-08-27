574193b9ceff3e4b00d5942cf50702e818c72b30
MEARZIM <asahaayan@gmail.com>
2025-08-27 23:18:17 +0530

    feat: add Single Track Page component with under construction message

---
+++ src/app/(routes)/artists/[artistId]/albums/[albumId]/tracks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleTrackPage = () => {
+  return (
+    <div className='flex items-center justify-center h-full text-2xl font-bold text-red-600'>
+        Page Under Construction
+    </div>
+  )
+}
+
+export default SingleTrackPage
